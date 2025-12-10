7a11474c2a7a2576a5ce96d556da09ec0a955bb3
kaustavGitH <101972989+kaustavGitH@users.noreply.github.com>
2025-12-10 22:43:02 +0530

    Create dockerPush.groovy

---
+++ vars/dockerPush.groovy
@@ -0,0 +1,8 @@
+def call(String projectName, String tagname, String dockerHubUser){
+  echo "This is pushing the image to docker hub"
+   withCredentials([usernamePassword(
+   'credentialsId':"dockerHubCred",
+   passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")]){
+   sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+   sh "docker push ${dockerHubUser}/${projectName}:${tagname}"
+}
