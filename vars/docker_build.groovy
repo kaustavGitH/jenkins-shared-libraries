cb13be6eadb99bb160d3b319c117242582285341
kaustavGitH <101972989+kaustavGitH@users.noreply.github.com>
2026-02-15 19:10:19 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,12 @@
-def call(String projectName, String tagname, String dockerHubUser){
-  echo "This is building the code"
-  sh "docker build -t ${dockerHubUser}/${projectName}:${tagname} ."
+def call(Map config = [:]) {
+    def imageName = config.imageName ?: error("Image name is required")
+    def imageTag = config.imageTag ?: 'latest'
+    def dockerfile = config.dockerfile ?: 'Dockerfile'
+    def context = config.context ?: '.'
+    
+    echo "Building Docker image: ${imageName}:${imageTag} using ${dockerfile}"
+    
+    sh """
+        docker build -t ${imageName}:${imageTag} -t ${imageName}:latest -f ${dockerfile} ${context}
+    """
 }
