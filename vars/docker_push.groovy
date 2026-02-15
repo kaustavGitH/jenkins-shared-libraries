f059a3d77cb51ed0b2102c058df300b843d9ea86
kaustavGitH <101972989+kaustavGitH@users.noreply.github.com>
2026-02-15 19:59:36 +0530

    Update tagname

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,8 +1,8 @@
-def call(String imageName, String tagname, String dockerHubUser){
+def call(String imageName, String tagName, String dockerHubUser){
   echo "Pushing image ${imageName}:${tagName} to docker hub"
    withCredentials([usernamePassword('credentialsId':"dockerHubCred",passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")]){
    sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
-   sh "docker push ${dockerHubUser}/${imageName}:${tagname}"
+   sh "docker push ${dockerHubUser}/${imageName}:${tagName}"
    sh "docker push ${dockerHubUser}/${imageName}:latest"
 }
 }
