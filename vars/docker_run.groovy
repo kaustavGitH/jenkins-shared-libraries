f69d5ab285761cb696aa3895c352bd9219cc4fc2
kaustavGitH <101972989+kaustavGitH@users.noreply.github.com>
2026-03-16 23:31:20 +0530

    Update docker_run.groovy

--- vars/docker_run.groovy
+++ vars/docker_run.groovy
@@ -2,12 +2,12 @@ def call(Map config = [:]){
   def imageName = config.imageName ?: error ("Image name is required")
   def hostPort = config.hostPort ?: error ("Host port is required")
   def containerPort = config.containerPort ?: error ("Container port is required")
-  def imageTag = config.imageTag ?: 
+  def imageTag = config.imageTag ?: 'latest'
   def dockerHubUser = config.dockerHubUser ?: error ("Dockerhub user is required")
 
-  echo "Running docker image: ${dockerHubUser}/${imageName}:
+  echo "Running docker image: ${dockerHubUser}/${imageName}:${imageTag}
 
   sh """
-      docker run -d -p ${hostPort}:${containerPort} ${imageName}
+      docker run -d -p ${hostPort}:${containerPort} ${dockerHubUser}/${imageName}:${imageTag}
   """
 }
